0d2d837a23eb54fab6ae626c0f4df127816bff36
MS_D_rei <msmdmission1111@gmail.com>
2024-05-26 20:19:45 +0900

    Add: explanation comments to showargs1.asm

--- chapter11/showargs1/showargs1.asm
+++ chapter11/showargs1/showargs1.asm
@@ -1,3 +1,11 @@
+; This showargs1.asm is for SASM.
+
+; Executable name		: showargs1
+; Version
+; Created date
+; Last update
+; Description
+
 section .data
 
 	ErrorMessage		db "Terminated with error.", 0x0a
@@ -12,29 +20,38 @@ section .text
 
 _start:
 	mov rbp, rsp
-	nop
 
-	mov r14, rsi                ; RSI contains the address of the first item in the list of cli args.
-	mov r15, rdi                ; RDI contains the number of cli args.
+	mov r14, rsi                ; RSI contains the address of the first item in the list of CLI args.
+	mov r15, rdi                ; RDI contains the number of CLI args.
 
-	cmp qword r15, MaxArgs      ; If the number of cli args > MaxArgs
-	ja Error
+	; cmp qword r15, MaxArgs      ; If the number of CLI args > MaxArgs
+	; ja Error
 
-	xor rbx, rbx
+	xor rbx, rbx                ; Set RBX 0 as a index for CLI args.
 
 Scan1:
-	xor rax, rax
-	mov rcx, 0x0ffff
-	mov rdi, qword [r14+rbx*8]
-	mov rdx, rdi
+	xor rax, rax                ; AL = 0.
+	mov rcx, 0x0ffff            ; Limit search count. 65,535.
+	mov rdi, qword [r14+rbx*8]  ; RDI = the address of CLI args.
+	mov rdx, rdi                ; RDX = the address of CLI args.
 
 	cld
-	repne scasb
+	repne scasb                 ; repne (Repeat while not equal) scasb (Scan String by Byte).
+	; Search for 0(null) in string at RDI.
+	; while (RCX >= 0)
+	; {
+	;    if [RDI] == AL return;
+	;    if [RDI] != AL
+	;        ++RDI;
+	;        --RCX;
+	; }
+	; Now RDI point the byte AFTER the found character's position.
 	jnz Error
 
-	mov byte [rdi-1], 10
-	sub rdi, rdx
-	mov r13, rdi
+	mov byte [rdi-1], 0x0a       ; Put EOL where the null is.
+	; Calculate the length of the arg.
+	sub rdi, rdx                 ; This is like equ rdi - rdx.
+	mov r13, rdi                 ; Preserve the length of the arg.
 
 	mov rax, 1
 	mov rdi, 1
@@ -42,10 +59,10 @@ Scan1:
 	mov rdx, r13
 	syscall
 
-	inc rbx
-	cmp rbx, r15
-	jb Scan1
-	jmp Exit
+	inc rbx                      ; Increment index of CLI args.
+	cmp rbx, r15                 ; Compare the index of CLI args, the number of CLI args.
+	jb Scan1                     ; If not the index reaches the number.
+	jmp Exit                     ;
 
 Error:
 	mov rax, 1

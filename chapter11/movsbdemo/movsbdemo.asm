107c5357f389aede5e80cf8acb0fc06cd8150144
MS_D_rei <msmdmission1111@gmail.com>
2024-06-11 17:47:09 +0900

    Add: explanation comment to movsb in movsbdemo.asm

--- chapter11/movsbdemo/movsbdemo.asm
+++ chapter11/movsbdemo/movsbdemo.asm
@@ -17,10 +17,19 @@ _start:
 
 	std                                      ; Set Direction Flag
 	mov rbx, EditBuffer
-	mov rsi, EditBuffer+EndPosition
 	mov rdi, EditBuffer+EndPosition+1
+	mov rsi, EditBuffer+EndPosition
 	mov rcx, EndPosition-InsertPosition+2
 	rep movsb
+	; while (RCX >= 0)
+	; {
+	;    RDI[rdiIndex] = RSI[rsiIndex];
+	;    --RCX;
+	;    ; rdiIndex and rsiIndex will be decremented
+	;    ; because `std` set direction flag.
+	;    --rdiIndex;
+	;    --rsiIndex;
+	; }
 
 	mov byte [rbx], 0x20
 
